d958e00f640dae0d3de93f087236a49900ef6cd3
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-08 17:18:46 +0700

    up file sort array

---
+++ sortArray.js
@@ -0,0 +1,16 @@
+function sort() {
+    let arrayNumber=[];
+    let number;
+    let temp;
+    for (let i = 0; i < 5; i++) {
+        number = Number(prompt("Import number"));
+        arrayNumber.push(number);
+    }
+    for(let i=0;i<arrayNumber.length/2;i++)
+    {
+        temp=arrayNumber[i];
+        arrayNumber[i]=arrayNumber[arrayNumber.length-1-i];
+        arrayNumber[arrayNumber.length-1-i]=temp;
+    }
+    document.write(arrayNumber);
+}
