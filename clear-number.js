313583509a1474ad6797443431327d5a7bc60a7c
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-09 08:41:59 +0700

    up file clear number in array

---
+++ clear-number.js
@@ -0,0 +1,27 @@
+function find() {
+    let limitArray = Number(prompt("Import limit Array"));
+    let number, index = 0, count = 0, value, flag = true;
+    let arrayNumber = [];
+    for (let i = 0; i < limitArray; i++) {
+        number = Number(prompt("Import number"));
+        arrayNumber.push(number);
+    }
+    alert(arrayNumber);
+    value = Number(prompt("Import number need find"));
+    for (let j = 0; j < arrayNumber.length; j++) {
+        if (arrayNumber[j] == value) {
+            flag = false;
+            index = j;
+            break;
+        }
+        count++;
+    }
+    if (count == arrayNumber.length) {
+        alert(value + " is not in array");
+        count = 0;
+    }
+    if (flag == false) {
+        arrayNumber.splice(index, 1);
+        alert(arrayNumber);
+    }
+}
